071840ad05f9626bee0bd9ed3516b9521b048516
Oleg Shikov <ol.shikov@yandex.ru>
2019-06-25 17:56:44 +0300

    Add navigation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //const ePub = require('epubjs');
 import ePub from 'epubjs';
 const file = require("./220.epub");
- window.onload = () => {
-     console.log(file)
+window.onload = () => {
+    console.log(file)
     var book = ePub(file);
-    var rendition = book.renderTo("area", { method: "default", flow: "scrolled-doc", width: '100vw', height: '100vh'});
-    var displayed = rendition.display(3);
+    var rendition = book.renderTo("area", { method: "default", flow: "scrolled-doc", width: '100vw', height: '100vh' });
+    var displayed = rendition.display();
     window.rendition = rendition;
- }
+}
